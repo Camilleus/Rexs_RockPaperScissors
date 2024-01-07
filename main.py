22e16edbee9ab7765ecd2d3a5c1e80e1a86e42a6
CamilleusRex <camilleusrex@gmail.com>
2024-01-07 19:31:13 +0100

    added some SOLID things

--- main.py
+++ main.py
@@ -2,78 +2,68 @@ import random
 from enum import IntEnum
 
 
-translations = {
-    'tie': "Both players selected {user_action.name}. It's a tie!",
-    'win': "{user_action.name} wins! {user_action.name} beats {computer_action.name}.",
-    'lose': "{user_action.name} loses. {computer_action.name} beats {user_action.name}.",
-}
-
-
 class Action(IntEnum):
     Rock = 0
     Paper = 1
     Scissors = 2
 
 
-def get_user_selection():
-    while True:
-        try:
-            choices = [f"{action.name}[{action.value}]" for action in Action]
-            choices_str = ", ".join(choices)
-            selection = int(input(f"Enter a choice ({choices_str}): "))
-            action = Action(selection)
-            return action
-        except (ValueError, IndexError):
-            range_str = f"[0, {len(Action) - 1}]"
-            print(f"Invalid selection. Enter a value in range {range_str}")
+class Game:
+    def __init__(self):
+        self.translations = {
+            'tie': "Both players selected {user_action.name}. It's a tie!",
+            'win': "{user_action.name} wins! {user_action.name} beats {computer_action.name}.",
+            'lose': "{user_action.name} loses. {computer_action.name} beats {user_action.name}.",
+        }
 
 
-def get_computer_selection():
-    selection = random.randint(0, len(Action) - 1)
-    action = Action(selection)
-    return action
+    def get_user_selection(self):
+        while True:
+            try:
+                choices = [f"{action.name}[{action.value}]" for action in Action]
+                choices_str = ", ".join(choices)
+                selection = int(input(f"Enter a choice ({choices_str}): "))
+                action = Action(selection)
+                return action
+            except (ValueError, IndexError):
+                range_str = f"[0, {len(Action) - 1}]"
+                print(f"Invalid selection. Enter a value in range {range_str}")
 
 
-def determine_winner(user_action, computer_action):
-    if user_action == computer_action:
-        print(f"Both players selected {user_action.name}. It's a tie!")
-    elif user_action == Action.Rock:
-        if computer_action == Action.Scissors:
-            print("Rock smashes scissors! You win!")
+    def get_computer_selection(self):
+        selection = random.randint(0, len(Action) - 1)
+        action = Action(selection)
+        return action
+
+
+    def determine_winner(self, user_action, computer_action):
+        if user_action == computer_action:
+            print(self.translations['tie'])
+        elif (user_action, computer_action) in [(Action.Rock, Action.Scissors), (Action.Paper, Action.Rock), (Action.Scissors, Action.Paper)]:
+            print(self.translations['win'])
         else:
-            print("Paper covers rock! You lose.")
-    elif user_action == Action.Paper:
-        if computer_action == Action.Rock:
-            print("Paper covers rock! You win!")
-        else:
-            print("Scissors cuts paper! You lose.")
-    elif user_action == Action.Scissors:
-        if computer_action == Action.Paper:
-            print("Scissors cuts paper! You win!")
-        else:
-            print("Rock smashes scissors! You lose.")
+            print(self.translations['lose'])
 
 
-def print_result(user_action, computer_action):
-    if user_action == computer_action:
-        print(translations['tie'])
-    elif (user_action, computer_action) in [(Action.Rock, Action.Scissors), (Action.Paper, Action.Rock), (Action.Scissors, Action.Paper)]:
-        print(translations['win'])
-    else:
-        print(translations['lose'])
+    def play(self):
+        while True:
+            try:
+                user_action = self.get_user_selection()
+            except ValueError as e:
+                range_str = f"[0, {len(Action) - 1}]"
+                print(f"Invalid selection. Enter a value in range {range_str}")
+                continue
+
+            computer_action = self.get_computer_selection()
+            self.determine_winner(user_action, computer_action)
+
+            play_again = input("Play again? (y/n): ")
+            if play_again.lower() != "y":
+                break
+
+
+if __name__ == "__main__":
+    game = Game()
+    game.play()
+
         
-        
-while True:
-    try:
-        user_action = get_user_selection()
-    except ValueError as e:
-        range_str = f"[0, {len(Action) - 1}]"
-        print(f"Invalid selection. Enter a value in range {range_str}")
-        continue
-
-    computer_action = get_computer_selection()
-    determine_winner(user_action, computer_action)
-
-    play_again = input("Play again? (y/n): ")
-    if play_again.lower() != "y":
-        break
