4c6279680c77267ca52a0ee2a36068c9904d8319
CamilleusRex <camilleusrex@gmail.com>
2024-01-07 19:58:24 +0100

    fixed typos and logic

--- main.py
+++ main.py
@@ -9,12 +9,16 @@ class Action(IntEnum):
 
 
 class Game:
+    
     translations = {
         'tie': "Both players selected {user_action.name}. It's a tie!",
         'win': "{user_action.name} wins! {user_action.name} beats {computer_action.name}.",
         'lose': "{user_action.name} loses. {computer_action.name} beats {user_action.name}.",
     }
+
+
     def __init__(self):
+        pass
 
 
     def get_user_selection(self):
@@ -45,23 +49,21 @@ class Game:
             print(self.translations['lose'])
 
 
-def play(self):
-    try:
-        while True:
-            user_action = self.get_user_selection()
-            computer_action = self.get_computer_selection()
-            self.determine_winner(user_action, computer_action)
+    def play(self):
+        try:
+            while True:
+                user_action = self.get_user_selection()
+                computer_action = self.get_computer_selection()
+                self.determine_winner(user_action, computer_action)
 
-            play_again = input("Play again? (y/n): ")
-            if play_again.lower() != "y":
-                break
-    except KeyboardInterrupt:
-        print("\nGame interrupted. Exiting.")
-        exit()
+                play_again = input("Play again? (y/n): ")
+                if play_again.lower() != "y":
+                    break
+        except KeyboardInterrupt:
+            print("\nGame interrupted. Exiting.")
+            exit()
 
 
 if __name__ == "__main__":
     game = Game()
     game.play()
-
-        
